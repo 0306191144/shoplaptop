3c199caa1cb95946d18bcae718d29edb252e704e
tranducminh <0306191144@caothang.edu.vn>
2022-01-25 23:32:27 +0700

    'l7'

--- flutter_shop/lib/provider/auth/registerprovider.dart
+++ flutter_shop/lib/provider/auth/registerprovider.dart
@@ -4,32 +4,30 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter_shop/Model/user.dart';
 
-late User userRegister;
-bool tam = true;
-String mess = '';
-
+// ignore: camel_case_types
 class registerProvider with ChangeNotifier {
+  late User userRegister;
+  bool tam = false;
+  String mess = '';
   Future<bool> register(String email, String name, String password,
       String passwordconfirmation) async {
-    final response =
-        await http.post(Uri.parse('http://192.168.1.7:8000/api/login'), body: {
+    final response = await http
+        .post(Uri.parse('http://192.168.1.7:8000/api/register'), body: {
       'email': email,
+      'name': name,
       'password': password,
       'password_confirmation': passwordconfirmation,
-      'name': name
     });
 
-    if (response.statusCode == 200) {
-      dynamic object = json.decode(response.body);
-      dynamic resutl = object['success'];
-      mess = object['message'];
-      tam = resutl;
-      if (tam == true) {
-        dynamic data = object['user'];
-        userRegister = User.fromJson(data);
-      }
+    dynamic object = await json.decode(response.body);
+    print(object);
+    dynamic resutl = object['success'];
+    tam = resutl;
+    if (tam == true) {
+      dynamic data = object['user'];
+      userRegister = User.fromJson(data);
     } else {
-      print('fail');
+      print('do file fail');
     }
     notifyListeners();
     return tam;
